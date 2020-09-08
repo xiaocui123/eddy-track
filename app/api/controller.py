0ffc755d4d47edf54971c46c3c78c03dec7fc74d
cuiguangqiang <cuiguangqiang@gmail.com>
2020-09-08 15:07:10 +0800

    加入经纬度范围

--- app/api/controller.py
+++ app/api/controller.py
@@ -17,7 +17,7 @@ def track():
     data = request.get_json() or {}
     if 'filepath' not in data:
         return bad_request('must include filepath fields')
-    if data['lon0'] is not None:
+    if data['lat0Index'] is not None:
         eddie = trackeddy(filepath=data['filepath'],lon0=data['lon0Index'],lon1=data['lon1Index'],lat0=data['lat0Index'],lat1=data['lat1Index'])
     else:
         eddie = trackeddy(filepath=data['filepath'])
